99aa49460d31297fd9b189636d844d0c9ca9760a
Richard Outten (mbp) <outtenr@duke.edu>
2011-03-11 13:23:58 -0500

    added solr indexer example

---
+++ src/main/scala/solr/SolrIndexer.scala
@@ -0,0 +1,35 @@
+package edu.duke.oit.vw.solr
+
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer
+import org.apache.solr.client.solrj.SolrServer
+import org.apache.solr.core.CoreContainer
+
+import java.io.File
+
+
+class SolrConfig(val file: String, val directory: String, val coreName: String)
+
+object Solr {
+  
+  def solrServer(solrConfig: SolrConfig):SolrServer = {
+    val configFile = new File( solrConfig.file )
+    val container = new CoreContainer
+
+    container.load( solrConfig.directory, configFile )
+    new EmbeddedSolrServer( container, solrConfig.coreName )
+  }
+
+}
+
+import org.apache.solr.client.solrj.beans.Field
+
+class Person {
+
+  @Field
+  var uri:String = _
+
+  @Field
+  var pubsJson:String = _
+
+}
+
